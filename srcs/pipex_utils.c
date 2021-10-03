c877c9aacb25a37496fa972bac85e4f95eb48a38
Sylsee <sylvio.poliart@gmail.com>
2021-10-03 05:13:57 +0200

    pipex

--- srcs/pipex_utils.c
+++ srcs/pipex_utils.c
@@ -6,13 +6,13 @@
 /*   By: spoliart <spoliart@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 02:54:56 by spoliart          #+#    #+#             */
-/*   Updated: 2021/10/03 03:55:15 by spoliart         ###   ########.fr       */
+/*   Updated: 2021/10/03 05:07:28 by spoliart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static char	*test_paths(char *cmd, char **paths, char *cmd_copy)
+static char	*test_paths(char *cmd, char **paths)
 {
 	int		i;
 	char	*path;
@@ -22,29 +22,45 @@ static char	*test_paths(char *cmd, char **paths, char *cmd_copy)
 	{
 		path = ft_strjoin(paths[i], cmd);
 		if (access(path, F_OK) == 0)
-			return (path);
+		{
+			if (access(path, X_OK) == 0)
+			{
+				errno = 0;
+				return (path);
+			}
+			errno = EACCES;
+		}
 		free(path);
 	}
-	if (access(cmd_copy, F_OK) == 0)
-		return (cmd_copy);
+	return (NULL);
+}
+
+char	*test_simple_path(char *cmd)
+{
+	if (access(cmd, F_OK | X_OK) == 0)
+		return (cmd);
 	return (NULL);
 }
 
 char	*get_path(char *cmd, char **environ)
 {
 	char	*path;
-	char	*cmd_copy;
 	char	**paths;
 
-	cmd_copy = cmd;
+	if (ft_strncmp(cmd, "./", 2) == 0)
+	{
+		path = test_simple_path(cmd);
+		dprintf(2, "slt\n");
+		if (!path)
+			dprintf(2, "errno: [%d]\n", errno);
+		return (NULL);
+	}
 	cmd = ft_strjoin("/", cmd);
-	while (*environ && ft_strncmp(*environ, "PATH=", 5))
+	while (environ && *environ && ft_strncmp(*environ, "PATH=", 5))
 		environ++;
 	paths = ft_split(*environ, "=:");
-	path = test_paths(cmd, paths, cmd_copy);
+	path = test_paths(cmd, paths);
 	free(cmd);
 	ft_free_tab(paths);
-	if (path)
-		return (path);
-	return (NULL);
+	return (path);
 }
